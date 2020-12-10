013bea3b02d3f0a9ad9f988d47eb58ed20451fb0
Vincent OLeary <vjoleary@users.noreply.github.com>
2020-12-10 18:10:50 -0500

    exercises for arrays and objects chapter

---
+++ script.js
@@ -0,0 +1,30 @@
+var highTemperatures = [55, 57, 53, 47, 56, 50];
+var lowTemperatures = [33, 30, 29, 35, 31, 32];
+
+console.log(highTemperatures[0]);
+console.log(lowTemperatures[5])
+
+let currentWeather = {
+    temperature: {
+        high: 28,
+        low: 12
+    },
+    conditions: "windy",
+    astronomy: {
+        sunrise: "7:43 AM",
+        sunset: "5:09 PM"
+    }
+}
+
+console.log(currentWeather.temperature.low)
+console.log("It's",currentWeather.temperature,"degrees F and",currentWeather.conditions)
+
+let likesToTravel = false
+if(likesToTravel === true){
+    console.log("Bon Voyage")
+} else{
+    console.log("Enjoy your couch")
+}
+
+let favoriteFood = "grilled cheese"
+if()
